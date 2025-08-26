57656b20a822adaa9ccc09dbdea5badfcdacf8a4
Bhatter1986 <yuvrajbhatter@gmail.com>
2025-08-27 00:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-
+from App.Routers import healthapp.include_router(health.router)
 <<<<<<< HEAD
 from App.Routers import health, instruments, optionchain, marketfeed, ai
 
